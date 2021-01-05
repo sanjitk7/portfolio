068e9344267190ffd7ce444314103c1bd07108f0
Sanjit Kumar <sanjitk2018@gmail.com>
2021-01-06 00:16:36 +0530

    added basic routing boiler plate

---
+++ src/components/Main/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { BrowserRouter as Switch, Route } from "react-router-dom";
+import Home from "../Home"
+const Main = ()=> {
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={Home}></Route>
+                {/* <Route path="/projects" component={}></Route> */}
+                {/* <Route path="/experience" component={Experience}></Route> */}
+            </Switch>
+        </div>
+    )
+}
+
+export default Main
