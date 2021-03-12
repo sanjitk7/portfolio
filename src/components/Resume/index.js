2c24962b8e6c95f70e6acbcf6ae042d96a540762
Sanjit Kumar <sanjitk2018@gmail.com>
2021-03-12 22:07:04 +0530

    fixed resume download

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,7 +7,7 @@ console.log("resume prop" , props)
       <section className="resume-section p-3 p-lg-5 d-flex justify-content-center" id="resume">
         <div className="w-100">
           <h2 className="mb-5">Resume</h2>
-          <div className="subheading mb-3"><a href="../../assets/SanjitCKS_Resume_Jan2020_v9" download="SanjitCKS_Resume_Jan2020_v9">Download Resume</a></div>
+          <div className="subheading mb-3"><a href="resume/SanjitKumar_ResumeFeb2021_v3.pdf" download="SanjitKumar_Resume">Download Resume</a></div>
         </div>
       </section>
     );
